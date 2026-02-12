0f72ce6f66e4cdfbcbd638bad27615d03bb18efe
nerenererr <dinrss22@gmail>
2026-02-12 10:29:40 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void tryCatchEj1() {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        try {
+            System.out.println("Please enter a number");
+            int num1 = sc.nextInt();
+            System.out.println("Please enter another number");
+            int num2 = sc.nextInt();
+            int result = num1 / num2;
+            System.out.println(result);
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number");
+        } catch (ArithmeticException e) {
+            System.out.println("The second number cannot be 0");
+        }
+
+    }
+
+    public static void tryCatchEj2() {
+
+        int[] numbers = {11, 22, 33};
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Please enter a number");
+            int index = sc.nextInt();
+            System.out.println("The number in the position " + index + " is " + numbers[index]);
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number");
+        } catch (ArrayIndexOutOfBoundsException e)  {
+            System.out.println("The number you entered is out of bounds");
+        }
+    }
+
+    public static void tryCatchEj3() {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            while ()
+
+        }
+    }
+}
